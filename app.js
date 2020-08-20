6f5d9e661bce5bd04628dd957175f7113831beee
TuyizeeAnastase <42033331+TuyizeeAnastase@users.noreply.github.com>
2020-08-20 20:09:31 +0200

    feature<CRUD>add CRUD operations[finishes:#5uhCz1Wo]

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import { async } from 'regenerator-runtime';
+import bodyParser, { urlencoded } from 'body-parser';
+
+import articleRouter from './routers/articleRouters';
+import userRouter from './routers/userRouters';
+
+const app=express();
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/MyBrand',
+{
+    useNewUrlParser:true,
+    useFindAndModify:false,
+    useUnifiedTopology:true
+})
+.then(()=>{
+    
+    console.log('database connected successfully')
+})
+.catch(err=>{
+    console.log(err)
+})
+
+app.use('/api/v1/blogs',articleRouter);
+app.use('/api/v1/users',userRouter);
+
+
+
+app.listen(3000,()=>{
+    console.log('server running');
+})
